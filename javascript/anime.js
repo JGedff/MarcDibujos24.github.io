1155fe60ba15d1102e1b5bb621b625d6f027f3be
MarcDibujos24 <131817174+MarcDibujos24@users.noreply.github.com>
2023-04-26 17:46:33 +0200

    Update anime.js

--- javascript/anime.js
+++ javascript/anime.js
@@ -6,7 +6,7 @@ const anime = {
         },
         {
             "src": "./img/Lawliet.jpg",
-            "title": "Lawliet.jpg",
+            "title": "Lawliet",
         },
         {
             "src": "./img/Detectiu Conan.jpg",
