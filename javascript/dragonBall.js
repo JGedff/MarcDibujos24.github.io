98d5452442bae304e9dbf4998f7ebe49f95e29a7
MarcDibujos24 <131817174+MarcDibujos24@users.noreply.github.com>
2023-04-27 09:23:19 +0200

    Add files via upload

--- javascript/dragonBall.js
+++ javascript/dragonBall.js
@@ -1,28 +1,22 @@
 const dragonBall = { // https://www.instagram.com/stories/highlights/17894459623491747/
     "doc": [
         {
-            "src": "./img/",
-            "title": "name",
+            "src": "./img/gokuSS4azul.jpg",
+            "title": "Goku SS4",
         },
+        {
+            "src": "./img/videl.jpg",
+            "title": "Videl",
+        },
+        {
+            "src": "./img/Gotenks.jpg",
+            "title": "Gotenks",
+        },
+        /*
         {
             "src": "./img/",
             "title": "name",
         },
-        {
-            "src": "./img/",
-            "title": "name",
-        },
-        {
-            "src": "./img/",
-            "title": "name",
-        },
-        {
-            "src": "./img/",
-            "title": "name",
-        },
-        {
-            "src": "./img/",
-            "title": "name",
-        }
+        */
     ]
 }
