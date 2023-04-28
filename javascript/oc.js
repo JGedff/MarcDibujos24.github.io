1bbc980357ce211598dab0113c53f3a91ea0c5bf
MarcDibujos24 <131817174+MarcDibujos24@users.noreply.github.com>
2023-04-28 09:55:32 +0200

    Add files via upload

--- javascript/oc.js
+++ javascript/oc.js
@@ -1,28 +1,86 @@
-const oc = { //https://www.instagram.com/stories/highlights/17986122217367148/
+const oc = {
     "doc": [
         {
-            "src": "./img/",
-            "title": "name",
+            "src": "./img/fantasmatico.jpg",
+            "title": "Fantasmático (Ben 10)",
         },
+        {
+            "src": "./img/bruja.jpeg",
+            "title": "Bruja",
+        },
+        {
+            "src": "./img/fondo2.jpeg",
+            "title": "Fondo de pantalla",
+        },
+        {
+            "src": "./img/animales.jpeg",
+            "title": "Animales",
+        },
+        {
+            "src": "./img/1.jpg",
+            "title": "Vampira",
+        },
+        {
+            "src": "./img/2.jpg",
+            "title": "Imagen 2",
+        },
+        {
+            "src": "./img/3.jpg",
+            "title": "Imagen 3",
+        },
+        {
+            "src": "./img/4.jpg",
+            "title": "Imagen 4",
+        },
+        {
+            "src": "./img/5.jpg",
+            "title": "Imagen 5",
+        },
+        {
+            "src": "./img/6.jpg",
+            "title": "Dragon de Sant Jordi",
+        },
+        {
+            "src": "./img/7.jpg",
+            "title": "Imagen 7",
+        },
+        {
+            "src": "./img/8.jpg",
+            "title": "Imagen 8",
+        },
+        {
+            "src": "./img/9.jpg",
+            "title": "Imagen 9",
+        },
+        {
+            "src": "./img/10.jpg",
+            "title": "Imagen 10",
+        },
+        {
+            "src": "./img/11.jpg",
+            "title": "Imagen 11",
+        },
+        {
+            "src": "./img/12.jpg",
+            "title": "Imagen 12",
+        },
+        {
+            "src": "./img/13.jpg",
+            "title": "Imagen 13",
+        },
+        {
+            "src": "./img/14.jpg",
+            "title": "Imagen 14",
+        },
+        {
+            "src": "./img/15.jpg",
+            "title": "Imagen 15",
+        },
+        /*
         {
             "src": "./img/",
             "title": "name",
         },
-        {
-            "src": "./img/",
-            "title": "name",
-        },
-        {
-            "src": "./img/",
-            "title": "name",
-        },
-        {
-            "src": "./img/",
-            "title": "name",
-        },
-        {
-            "src": "./img/",
-            "title": "name",
-        }
+        */
     ]
 }
